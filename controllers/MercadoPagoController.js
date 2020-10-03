a99916242a7cce51e57d80446e6f72cdd1fe5b40
Federico Quattrocchio <federicoquattrocchio@gmail.com>
2020-10-03 16:45:50 -0300

    Cambio para webhook

--- controllers/MercadoPagoController.js
+++ controllers/MercadoPagoController.js
@@ -11,18 +11,8 @@ class MercadoPagoController {
     }
 
     processIPN = async (req, res) => {
-        if (req.method === "POST") { 
-            let body = ""; 
-            req.on("data", chunk => {  
-                body += chunk.toString();
-            });
-            req.on("end", () => {  
-                console.log(body, "webhook response"); 
-                res.end("ok");
-            });
-        }
-        return res.status(200); 
-        // res.json(req.body);
+        console.log(req.body);
+        res.json(req.body);
     }
 }
 
